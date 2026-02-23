87f93758d01b3dd2e7549d864b798835052d96ba
Aleem <aleemmuhammad93@gmail.com>
2026-02-23 12:31:46 +0500

    code optimization

--- Tickets/urls.py
+++ Tickets/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("list/all/", views.list_all_tickets),
 
     #update ticket staus
-    path("update-ticket-status/", views.update_ticket_status, name="update_ticket_status"),
+    # path("update-ticket-status/", views.update_ticket_status, name="update_ticket_status"),
 
     # History by employee_id 
     path("ticket-history/<int:employee_id>/", views.ticket_history, name="ticket-history"),
@@ -28,27 +28,13 @@ urlpatterns = [
 
     
 
-    # ticket approval or rejection dynamically
-    # path('action/<int:ticket_id>/', ticket_action, name="ticket_action"),
-
-    # Manual escalation
-    # path("escalate/<int:ticket_id>/", views.escalate_ticket, name="escalate_ticket"),
-
-    # Email process actions
-    # path("team-pmo-action/<int:ticket_id>/", views.team_pmo_action),
-    # path("admin-complete/<int:ticket_id>/", views.admin_complete),
-
-    # ✅ Workflow Management APIs (for Postman + Admin-like control)
+    # list of workflow
     path("workflows/", views_workflow.list_workflows, name="list_workflows"),
-    path("workflows/create/", views_workflow.create_workflow, name="create_workflow"),
-    path("workflows/<int:workflow_id>/steps/add/", views_workflow.add_workflow_step, name="add_workflow_step"),
-    path("workflows/<int:workflow_id>/activate/", views_workflow.activate_workflow, name="activate_workflow"),
-
-     # ✅ Workflow fecching first role base on is_active is true
-    path("workflows/active/step1-role/", views_workflow.active_workflow_step1_role, name="active_step1_role"),
-
-
+    
+    # ticket Assigned dashbaord
     path('dashboard/<int:user_id>/', views.dashboard_tickets, name='dashboard_tickets'),
 
+
+    #Action on ticket Approve | Reject
     path('action/<int:ticket_id>/', views.ticket_action, name='ticket_action'),
 ]
